86414c72540c8dad91ac62bdd41512072591eae1
Baig <hilal.beg@gmail.com>
2024-12-20 15:53:14 +0500

    Update HeadsetManager.kt

--- android/src/main/kotlin/com/github/florent37/assets_audio_player/stopwhencall/HeadsetManager.kt
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/stopwhencall/HeadsetManager.kt
@@ -90,7 +90,8 @@ class HeadsetManager(private val context: Context) {
     fun Context.hasPermissionBluetooth() : Boolean {
         try {
             val packageInfo = this.packageManager.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS)
-            return  packageInfo.requestedPermissions.contains("android.permission.BLUETOOTH")
+            return  packageInfo.requestedPermissions?.contains("android.permission.BLUETOOTH") == true
+
         } catch (t: Throwable) {
 
         }
